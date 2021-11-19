e0f83e9edc3fb968d7cd594bdaa2953a2da87ed2
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-11-19 22:52:16 +0600

    Complete Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Post from "./pages/Post";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Post />
+      <Login />
     </div>
   );
 }
