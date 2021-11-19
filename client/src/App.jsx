c64a58a9afc91045ae46469d9ec74c766efd86ef
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-11-19 22:20:31 +0600

    Complete Post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Post from "./pages/Post";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Post />
     </div>
   );
 }
