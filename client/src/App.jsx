5a084828ff44a94e903bd40bc424f96d6c73dd2c
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-11-21 22:32:29 +0600

    Complete Router and Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,27 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Post from "./pages/Post";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div>
-      <Navbar />
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar user={user} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/post/:id"
+            element={user ? <Post /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
   return (
     <div className="navbar">
-      <span className="logo">Dev App</span>
-      <ul className="list">
-        <li className="listItem">
-          <img
-            src="https://images.pexels.com/photos/9968409/pexels-photo-9968409.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            alt=""
-            className="avatar"
-          />
-        </li>
-        <li className="listItem">John Doe</li>
-        <li className="listItem">Logout</li>
-      </ul>
+      <span className="logo">
+        {" "}
+        <Link to="/" className="link">
+          Dev App
+        </Link>
+      </span>
+      {user ? (
+        <ul className="list">
+          <li className="listItem">
+            <img
+              src="https://images.pexels.com/photos/9968409/pexels-photo-9968409.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+              className="avatar"
+            />
+          </li>
+          <li className="listItem">John Doe</li>
+          <li className="listItem">Logout</li>
+        </ul>
+      ) : (
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      )}
     </div>
   );
 };
